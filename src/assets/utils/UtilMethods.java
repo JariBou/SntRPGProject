6202bf88a7f40edecdf841f59979e96cd8f2c185
Jari <bourdietome@gamil.com>
2022-08-07 20:38:22 +0200

    start to work on Change on file loading

--- src/assets/utils/UtilMethods.java
+++ src/assets/utils/UtilMethods.java
@@ -1,12 +1,41 @@
 package assets.utils;
 
 
+import java.io.InputStream;
+import org.apache.commons.io.IOUtils;
+
+
 @SuppressWarnings("rawtypes")
 public class UtilMethods {
 
     public static String getType(Class c){
         String[] s = String.valueOf(c).split("\\.");
         return s[s.length - 1];
+    }
+
+    public static String getFile(String fileName)
+    {
+        //Get file from resources folder
+        ClassLoader classLoader = UtilMethods.class.getClassLoader();
+
+        InputStream stream = classLoader.getResourceAsStream(fileName);
+
+        try
+        {
+            if (stream == null)
+            {
+                throw new Exception("Cannot find file " + fileName);
+            }
+
+            return IOUtils.toString(stream);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+
+            System.exit(1);
+        }
+
+        return null;
     }
 
 }
